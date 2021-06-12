1899c04a872a5c18b08418e8d1335dddcd579145
fahadaleem <faleem396@gmail.com>
2021-06-12 21:37:10 +0500

    Applicants APIs done

--- mytutor/__init__.py
+++ mytutor/__init__.py
@@ -4,8 +4,9 @@ from config import Config
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 app.config.from_object(Config)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 
 db = SQLAlchemy(app)
