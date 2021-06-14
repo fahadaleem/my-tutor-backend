0a0aeeab9693d2b371b7fce0c4ebd9f52fdeac6b
fahadaleem <faleem396@gmail.com>
2021-06-14 14:03:16 +0500

    committed

--- mytutor/__init__.py
+++ mytutor/__init__.py
@@ -11,7 +11,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://oimevbslxmdpse:eff201d6e4f189b0419cde1cf66e1b1a54af7f99f2ff7e22f3629eb296989482@ec2-35-171-250-21.compute-1.amazonaws.com:5432/dcc47gddmqf9s'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://oimevbslxmdpse:eff201d6e4f189b0419cde1cf66e1b1a54af7f99f2ff7e22f3629eb296989482@ec2-35-171-250-21.compute-1.amazonaws.com:5432/dcc47gddmqf9s'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
