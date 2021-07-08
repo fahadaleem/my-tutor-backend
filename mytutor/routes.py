c40edb9ef8c09ccebbe77149c0f69c4f2c331504
fahadaleem <faleem396@gmail.com>
2021-07-08 06:05:39 +0500

    Some APIS Fixed

--- mytutor/routes.py
+++ mytutor/routes.py
@@ -352,9 +352,9 @@ def add_new_review():
 def get_course_details():
     try:
         course_id = request.args.get('course_id')
-        teacher_id = request.args.get('teacher_id')
+        # teacher_id = request.args.get('teacher_id')
         course_info = db.session.query(Teachers.id, Teachers.name.label('teacher_name'), Teachers.teaching_experience, Teachers.gender,Courses.name, Courses.title, Courses.description, Courses.price, Courses.course_outline, Courses.duration,Courses.category, Course_Assign).join(Teachers).join(Courses).filter(Courses.id==course_id).one()
-        reviews_info = Reviews.query.filter(and_(Reviews.course_id==course_id, Reviews.teacher_id==teacher_id)).all()
+        reviews_info = Reviews.query.filter(and_(Reviews.course_id==course_id, Reviews.teacher_id==course_info.id)).all()
 
         return generate_json_for_course_details(course_info, reviews_info)
     except SQLAlchemyError as e:
