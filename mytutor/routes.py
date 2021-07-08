e8121d5ae936f870bcc6cf8598cbae983d711cb8
fahadaleem <faleem396@gmail.com>
2021-07-09 04:19:26 +0500

    Course Outlined Fetch from Get all courses API

--- mytutor/routes.py
+++ mytutor/routes.py
@@ -360,13 +360,13 @@ def add_new_review():
 
 
 
-## localhost:2000/get-all-details?course_id=PY-01&&teacher_id=1
+## localhost:2000/get-course-details?course_id=PY-01
 @app.route("/get-course-details", methods=['GET'] )
 def get_course_details():
     try:
         course_id = request.args.get('course_id')
         # teacher_id = request.args.get('teacher_id')
-        course_info = db.session.query(Teachers.id, Teachers.name.label('teacher_name'), Teachers.teaching_experience, Teachers.gender,Courses.name, Courses.title, Courses.description, Courses.price, Courses.course_outline, Courses.duration,Courses.category, Course_Assign).join(Teachers).join(Courses).filter(Courses.id==course_id).one()
+        course_info = db.session.query(Teachers.id, Teachers.name.label('teacher_name'), Teachers.teaching_experience, Teachers.gender,Courses.name, Courses.title, Courses.description,Courses.course_outline, Courses.price, Courses.course_outline, Courses.duration,Courses.category, Course_Assign).join(Teachers).join(Courses).filter(Courses.id==course_id).one()
         reviews_info = Reviews.query.filter(and_(Reviews.course_id==course_id, Reviews.teacher_id==course_info.id)).all()
 
         return generate_json_for_course_details(course_info, reviews_info)
